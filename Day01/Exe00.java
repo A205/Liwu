dd64f05fca7bdd2f56f765765adddae6663b8b3b
you-zi <1249287411@qq.com>
2015-07-07 19:04:02 +0800

    Day01

--- Day01/Exe00.java
+++ Day01/Exe00.java
@@ -1,12 +1,15 @@
 package lianxi;
 
+import java.util.Scanner;
+
 public class Exe00 {
 
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name;
-		name="XXXX";
+		Scanner input=new Scanner(System.in);
+		System.out.println("«Î ‰»Î–’√˚");
+		String name=input.next();
 		System.out.println("Hello "+name);
 		
 	}
